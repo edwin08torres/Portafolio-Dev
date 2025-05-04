bc3222b74049bc9cb0c8d6cbe95653e4429863db
edwin08torres <at2899743@gmail.com>
2025-05-03 20:23:24 -0600

    clean code

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,6 @@ export const Footer = () => {
   return (
     <footer className="bg-slate-950 text-slate-300 py-16 px-4 md:px-20">
       <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* CTA */}
         <div>
           <h3 className="text-xl font-bold text-white mb-2">¿Hablamos?</h3>
           <p className="text-sm">
@@ -18,7 +17,6 @@ export const Footer = () => {
           </a>
         </div>
 
-        {/* Información de contacto */}
         <div>
           <h3 className="text-xl font-bold text-white mb-2">Contacto</h3>
           <ul className="space-y-2 text-sm">
@@ -41,7 +39,6 @@ export const Footer = () => {
           </ul>
         </div>
 
-        {/* Redes sociales */}
         <div>
           <h3 className="text-xl font-bold text-white mb-2">Sígueme</h3>
           <div className="flex space-x-4 text-2xl mt-2">
